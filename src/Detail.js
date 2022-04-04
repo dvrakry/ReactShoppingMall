b5ee79fc1d995c00abf56b746b6dc88a941a6093
rak <dv.rakry@gmail.com>
2022-04-04 15:12:36 +0900

    2022-04-04

--- src/Detail.js
+++ src/Detail.js
@@ -1,87 +1,128 @@
 /* eslint-disable */
 
-
-import React, { useContext, useEffect, useState } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import styled from 'styled-components';
-import './Detail.scss';
-import {재고context} from './App.js';
+import React, { useContext, useEffect, useState } from "react";
+import { Navbar, Container, Nav, NavDropdown, Button, NavItem, NavLink } from 'react-bootstrap';
+import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "./Detail.scss";
+import { 재고context } from "./App.js";
+import {CSSTransition} from "react-transition-group";
 
 let 박스 = styled.div`
-  padding : 20px;
+  padding: 20px;
 `;
 
 let 제목 = styled.h4`
-  font-size : 25px;
-  color : ${ props => props.색상 }
+  font-size: 25px;
+  color: ${(props) => props.색상};
 `;
 
 
+function Detail(props) {
+  
+  let [alert, alert변경] = useState(true);
+  let [inputData, inputData변경] = useState("");
+  let 재고 = useContext(재고context);
+  let [누른탭, 누른탭변경] = useState(0);
+  let [스위치, 스위치변경] = useState(false);
 
+  useEffect(() => {
+    let 타이머 = setTimeout(() => {
+      alert변경(false);
+    }, 2000);
+    // return function 어쩌구(){}
+    return () => {
+      clearTimeout(타이머);
+    };
+  }, []);
 
+  let { id } = useParams();
+  let history = useHistory();
+  let 찾은상품 = props.shoes.find(function (상품) {
+    return 상품.id == id;
+  });
 
-function Detail(props){
-    let[alert, alert변경] = useState(true);
-    let[inputData, inputData변경] = useState('');
-    let 재고 = useContext(재고context);
+  return (
+    <div className="container">
+      <박스>
+        <제목 className="red">상세페이지</제목>
+      </박스>
 
-    useEffect(()=>{
-
-      let 타이머 = setTimeout(()=>{ alert변경(false) }, 2000)
-      // return function 어쩌구(){}
-      return ()=>{ clearTimeout(타이머) }
-    },[]);
-
-    
-
-    let { id } = useParams();
-    let history = useHistory();
-    let 찾은상품 = props.shoes.find(function(상품){
-        return 상품.id == id; 
-    })
-
-    return(
-        <div className="container">
-          <박스>
-            <제목 className='red'>상세페이지</제목>
-          </박스>
-
-          
-          {
-            alert == true
-            ? <div className='my-alert'>
-               <p>재고가 얼마 남지 않았습니다.</p>
-              </div>
-            : null
-          }
-          
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">{찾은상품.title}</h4>
-              <p>{찾은상품.content}</p>
-              <p>{찾은상품.price}</p>
-
-              <Info 재고 ={props.재고}></Info>  
-
-              <button className="btn btn-danger" onClick={()=>{ props.재고변경([9,11,12]) }}>주문하기</button> 
-              <button className="btn btn-primary" onClick={()=>{
-                history.goBack();
-                // history.push('/') 이거도 가능;  
-              }}>뒤로가기</button> 
-            </div>
-          </div>
+      {alert == true ? (
+        <div className="my-alert">
+          <p>재고가 얼마 남지 않았습니다.</p>
         </div>
-    )
+      ) : null}
+
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
+
+          <Info 재고={props.재고}></Info>
+
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              props.재고변경([9, 11, 12]);
+            }}
+          >
+            주문하기
+          </button>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              history.goBack();
+              // history.push('/') 이거도 가능;
+            }}
+          >
+            뒤로가기
+          </button>
+        </div>
+      </div>
+
+      <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+        <Nav.Item>
+          <Nav.Link eventKey="link-0" onClick={()=>{스위치변경(false); 누른탭변경(0)}}>Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" onClick={()=>{스위치변경(false); 누른탭변경(1)}}>Option 2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <CSSTransition in={스위치} classNames="wow" timeout={500}>
+        <TabContent 누른탭 = {누른탭} 스위치변경={스위치변경}></TabContent>
+      </CSSTransition>      
+
+    </div>
+  );
 }
 
+function TabContent(props){
 
-function Info(props){
-  return(
-    <p>재고 : {props.재고[0]} </p>
-  )
+  useEffect(()=>{
+    props.스위치변경(true);
+  })
+
+  if(props.누른탭 === 0){
+    return <div> 0번째 내용입니다.</div>
+  } else if(props.누른탭 === 1){
+    return <div> 1번째 내용입니다.</div>
+  } else if(props.누른탭 === 2){
+    return <div> 2번째 내용입니다.</div>
+  }
+
+}
+
+function Info(props) {
+  return <p>재고 : {props.재고[0]} </p>;
 }
 
 export default Detail;
